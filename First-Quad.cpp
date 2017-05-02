cdd150543fd0f8e2529f2dbc07186f69e46ea60f
GeordieR <GeordieR@users.noreply.github.com>
2017-05-02 18:18:34 +1200

    Update First-Quad.cpp
    Included the plan for the first quadrant that was thought of in the lab Week 1

--- First-Quad.cpp
+++ First-Quad.cpp
@@ -1,1 +1,11 @@
 /* Overall plan for first quad
+
+Take a picture with the camera
+Take every tenth pixel and put it's rbg sum(4) in an array
+For loop to find max and min in array
+Take the max+min/2 and store in the threashold
+Convert the array using the threshold into booleans of black and white
+Go through the array and find a large grouping of white values
+Return the start and end position of the group, find the middle and calculate the real world angle from the real world centre
+Turn the robot that distance(add in another check for when debuging)
+Go forward a set distance and then run the check again*/
