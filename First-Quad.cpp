3284b4043981d52737e49b96941b273844e5bea2
GeordieR <GeordieR@users.noreply.github.com>
2017-05-08 11:15:42 +1200

    Update First-Quad.cpp
    Corrected pixel values and array sizes

--- First-Quad.cpp
+++ First-Quad.cpp
@@ -10,17 +10,17 @@ int lineDirection(){
 	int max = 0;    //Max rgb value
 	int min = 0;    //Min rgb value
 	int tol = 0;    //Tolerance for b/w values
-	int pix[240];   //Array for pixel rgb values
-	int w[240];     //Array to store white(255) or black(0)
+	char pix[320];   //Array for pixel rgb values
+	char w[320];     //Array to store white(255) or black(0)
 	//Gets pixel rgb values and stores them in array pix[]
 	take_picture();
-	while(cnt<240){
-		pix[cnt] = get_pixel(320,cnt+1,4);
+	while(cnt<320){
+		pix[cnt] = get_pixel(120,cnt+1,4);
 		cnt=cnt+1;
 	}
 	//Need to find the correct way to use a for loop for values in an array
 	//Finds the min and max to calculate tolerance value
-	for(int a = 0;a>240;a++){
+	for(int a = 0;a>320;a++){
 		if(pix[a]>max){
 			max = pix[a];
 		} else if(pix[a]<min){
@@ -29,7 +29,7 @@ int lineDirection(){
 	}
 	tol = (max+min)/2;
 	//Determines if black(0) or white(255) for pixels in pix[]
-	for(int a = 0;a>240;a++){
+	for(int a = 0;a>320;a++){
 		if(pix[a]>tol){
 			w[a] = 255;
 		}
@@ -38,7 +38,7 @@ int lineDirection(){
 		}
 	}
 	//Caclulates the error value for white line location
-	for(int a = 0;a>240;a++){
+	for(int a = 0;a>320;a++){
 		sum = sum + (w[a]*mlt);
 		mlt++;	
 	}
