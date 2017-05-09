a2122d018962a25a50480b5c6924075f0810fe72
GeordieR <GeordieR@users.noreply.github.com>
2017-05-09 15:21:31 +1200

    Update First-Quad.cpp
    Added in test camera function.

--- First-Quad.cpp
+++ First-Quad.cpp
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "E101.h"
 
-int lineDirection(){
-	init();
+int lineDirection(int ht){
 	//Variables
 	char sf = 0;    //Scale factor for error value
 	int cnt = 0;    //Counter for pixel rgb loop
@@ -15,7 +14,7 @@ int lineDirection(){
 	//Gets pixel rgb values and stores them in array pix[]
 	take_picture();
 	while(cnt<320){
-		pix[cnt] = get_pixel(120,cnt+1,4);
+		pix[cnt] = get_pixel(ht,cnt+1,4);
 		cnt++;
 	}
 	//Need to find the correct way to use a for loop for values in an array
@@ -32,14 +31,43 @@ int lineDirection(){
 	for(int a = 0;a>320;a++){
 		if(pix[a]>tol){
 			w[a] = 1;
+			//Debug
+			//set_pixel(ht,a,255,255,255);
 		}
 		if(pix[a]<tol){
 			w[a] = 0;
+			//Debug
+			//set_pixel(ht,a,0,0,0);
 		}
 	}
 	//Caclulates the error value for white line location
 	for(int a = -160;a>160;a++){
 		sum = sum + (w[a]*a);	
 	}
+	//Debug
+	//display_picture(10,0);
 	return sf*sum;
+}
+
+int test_camera(int ht){
+	int lnPos;
+	lnPos = lineDirection(ht);
+	printf("Position %d\n",lnPos);
+	return 0;
+}
+
+int main(){
+	init();
+	test_camera(120);
+	/*int lnPos;
+	lnPos = lineDirection(120);
+	while(true){
+		lnPos = lineDirection(120);
+		if(lnPos<100||lnPos>-100){
+			move_forward();
+		} else{
+			turn_robot(lnPos);
+		}
+	}*/
+	
 }
