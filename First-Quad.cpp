2ca63983bffee7f00c186063e04056ac9379a5cf
GeordieR <GeordieR@users.noreply.github.com>
2017-05-11 15:26:42 +1200

    Update First-Quad.cpp

--- First-Quad.cpp
+++ First-Quad.cpp
@@ -3,7 +3,7 @@
 //Line direction needs to sample every tenth pixel instead to spped up the calculation
 int lineDirection(int ht){
 	//Variables
-	char sf = 1;    //Scale factor for error value
+	double sf = 7.96875;    //Scale factor for error value
 	int cnt = 0;     //Counter for pixel rgb loop
 	signed int error = 0;    //Sum for white line locator
 	int max = 0;    //Max rgb value
@@ -11,7 +11,7 @@ int lineDirection(int ht){
 	int tol = 0;    //Tolerance for b/w values
 	char pix[32];   //Array for pixel rgb values
 	char w[32];     //Array to store white(255) or black(0)
-	//Gets pixel rgb values and stores them in array pix[]
+	//Gets pixel rgb values and stores them in array pix[]+
 	take_picture();
 	while(cnt<32){
 		pix[cnt] = get_pixel(ht,(cnt+1)*10,3);
@@ -34,21 +34,22 @@ int lineDirection(int ht){
 		if(pix[a]>tol){
 			w[a] = 1;
 			//Debug
-			//set_pixel(ht,a,255,255,255);
-			//printf("White Pixel");
+			set_pixel(ht,a,255,255,255);
+			printf("White Pixel\n");
 		}else if(pix[a]<tol){
 			w[a] = 0;
 			//Debug
-			//set_pixel(ht,a,1,1,1);
-			//printf("Black Pixel");
+			set_pixel(ht,a,1,1,1);
+			printf("Black Pixel\n");
 		}
 	}
 	//Caclulates the error value for white line location
-	for(signed int a = -16;a<16;a++){
+	for(int a = -16;a<16;a++){
 		error = error + (w[a]*a);	
 	}
 	//Debug
-	//display_picture(10,0);
+	
+	printf("%d",error);
 	return sf*error;
 }
 
@@ -59,6 +60,32 @@ void set_motor_left(int speed){
 void set_motor_right(int speed){
 	set_motor(2, -speed);
 }
+
+  		 
+    //make robot go to forward
+   	 int drive_forward(){
+		 printf("Forward");
+   		 set_motor_left(255);
+   		 set_motor_right(255);
+   		 sleep1(0,500000);
+   		 return 0;}
+   	 
+    //make robot go to left
+   	 int turn_left(int error){
+		 printf("Left");
+   		 set_motor_left(255);
+   		 //set_motor_right(sf * position);
+   		 sleep1(0,500000);
+   		 return 0;}
+   		 
+   		 
+    //make robot got to right
+   	 int turn_right(int error){
+		 printf("right");
+   		 //set_motor_left(sf * position);
+   		 set_motor_right(255);
+   		 sleep1(0,500000);
+   		 return 0;}
 
 
 
@@ -72,34 +99,14 @@ void test_camera(int ht){
    	 int main(){
    		 init();
 		 int error;
-		 while(true){
+		 //while(true){
 			error = lineDirection(120);
-			if(error>10){turn_left(error);}
-   		 	if(error<-10){turn_right(error);}
-   		 	if(-10<error<10){drive_forward;} 
+			display_picture(5,0);
+			//if(error>10){turn_left(error);}
+   		 	//if(error<-10){turn_right(error);}
+   		 	//if(-10<error&&error<10){drive_forward();} 
    		 	
-		 }
+		// }
    		 
    		 return 0;}
-   		 
-    //make robot go to forward
-   	 int drive_forward(){
-   		 set_motor_left(255);
-   		 set_motor_right(255);
-   		 sleep1(0,500000);
-   		 return 0;}
-   	 
-    //make robot go to left
-   	 int turn_left(int error){
-   		 set_motor_left(255);
-   		 //set_motor_right(sf * position);
-   		 sleep1(0,500000);
-   		 return 0;}
-   		 
-   		 
-    //make robot got to right
-   	 int turn_right(int error){
-   		 //set_motor_left(sf * position);
-   		 set_motor_right(255);
-   		 sleep1(0,500000);
-   		 return 0;}
+ 
