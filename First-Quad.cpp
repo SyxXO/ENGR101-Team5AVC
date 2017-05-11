bd312e5f648329a471f32c171631d0873ca74e84
GeordieR <GeordieR@users.noreply.github.com>
2017-05-11 15:59:49 +1200

    Finished code for the first quadrant
    S

--- First-Quad.cpp
+++ First-Quad.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "E101.h"
-//Line direction needs to sample every tenth pixel instead to spped up the calculation
+
 int lineDirection(int ht){
 	//Variables
-	double sf = 7.96875;    //Scale factor for error value
+	double sf = 1;    //Scale factor for error value
 	int cnt = 0;     //Counter for pixel rgb loop
-	signed int error = 0;    //Sum for white line locator
+	int error = 0;    //Sum for white line locator
 	int max = 0;    //Max rgb value
 	int min = 0;    //Min rgb value
 	int tol = 0;    //Tolerance for b/w values
@@ -15,10 +15,8 @@ int lineDirection(int ht){
 	take_picture();
 	while(cnt<32){
 		pix[cnt] = get_pixel(ht,(cnt+1)*10,3);
-		//printf("Got pixel %d\n",cnt);
 		cnt++;
 	}
-	//Need to find the correct way to use a for loop for values in an array
 	//Finds the min and max to calculate tolerance value
 	for(int a = 0;a<32;a++){
 		//printf("Stepped into pixel to white conversion loop");
@@ -33,19 +31,21 @@ int lineDirection(int ht){
 	for(int a = 0;a<32;a++){
 		if(pix[a]>tol){
 			w[a] = 1;
-			//Debug
-			set_pixel(ht,a,255,255,255);
 			printf("White Pixel\n");
 		}else if(pix[a]<tol){
 			w[a] = 0;
-			//Debug
-			set_pixel(ht,a,1,1,1);
 			printf("Black Pixel\n");
 		}
 	}
+	for(int a =0;a<32;a++){
+		printf("%d = %d\n",a,w[a]);
+	}
+	
 	//Caclulates the error value for white line location
+	cnt = 0;
 	for(int a = -16;a<16;a++){
-		error = error + (w[a]*a);	
+		error = error + (w[cnt]*a);
+		cnt++;	
 	}
 	//Debug
 	
@@ -62,38 +62,32 @@ void set_motor_right(int speed){
 }
 
   		 
-    //make robot go to forward
-   	 int drive_forward(){
-		 printf("Forward");
-   		 set_motor_left(255);
-   		 set_motor_right(255);
-   		 sleep1(0,500000);
-   		 return 0;}
+//make robot go to forward
+int drive_forward(){
+	printf("Forward");
+   	set_motor_left(255);
+   	set_motor_right(255);
+   	sleep1(0,500000);
+   	return 0;}
    	 
-    //make robot go to left
-   	 int turn_left(int error){
-		 printf("Left");
-   		 set_motor_left(255);
-   		 //set_motor_right(sf * position);
-   		 sleep1(0,500000);
-   		 return 0;}
+//make robot go to left
+int turn_left(int error){
+	printf("Left");
+   	set_motor_left(255);
+   	//set_motor_right(sf * position);
+   	sleep1(0,500000);
+   	return 0;}
    		 
    		 
-    //make robot got to right
-   	 int turn_right(int error){
-		 printf("right");
-   		 //set_motor_left(sf * position);
-   		 set_motor_right(255);
-   		 sleep1(0,500000);
-   		 return 0;}
+//make robot got to right
+int turn_right(int error){
+	printf("right");
+   	//set_motor_left(sf * position);
+   	set_motor_right(255);
+   	sleep1(0,500000);
+   	return 0;}
 
 
-
-void test_camera(int ht){
-	int lnPos;
-	lnPos = lineDirection(ht);
-	printf("Position %d\n",lnPos);
-}
 
 //movemont of robot
    	 int main(){
