3a1e42348d361db67149ee5a02f40c1cf288aab9
Geordie Rogers <GeordieR@users.noreply.github.com>
2017-05-18 15:37:22 +1200

    Create wifi_test.cpp

---
+++ wifi_test.cpp
@@ -0,0 +1,31 @@
+include <stdio.h>
+#include "E101.h"
+int main(){
+        char ip[15];
+        char please[24];
+        please[0] = 'P';
+        please[1] = 'l';
+        please[2] = 'e';
+        please[3] = 'a';
+        please[4] = 's';
+        please[5] = 'e';
+        ip[0] = '1';
+        ip[1] = '3';
+        ip[2] = '0';
+        ip[3] = '.';
+        ip[4] = '1';
+        ip[5] = '9';
+        ip[6] = '5';
+        ip[7] = '.';
+        ip[8] = '6';
+        ip[9] = '.';
+        ip[10] = '1';
+        ip[11] = '9';
+        ip[12] = '6';
+        char password[30];
+        connect_to_server(ip,1024);
+        send_to_server(please);
+        receive_from_server(password);
+        send_to_server(password);
+        return 0;
+}
